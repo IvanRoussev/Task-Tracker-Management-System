4848fc9d69938548ad0f59787d1e67651d437159
Ivan Roussev <ivan.roussev12@gmail.com>
2023-09-11 20:50:22 -0700

    implementing swagger

--- pkg/routes/taskmanager-controller.go
+++ pkg/routes/taskmanager-controller.go
@@ -3,13 +3,15 @@ package routes
 import (
 	"github.com/IvanRoussev/taskManager/pkg/controllers"
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 var RegisterTaskManagerRoutes = func(router *mux.Router) {
-	router.HandleFunc("/task/", controllers.GetTask).Methods("GET")
-	router.HandleFunc("/task/", controllers.CreateTask).Methods("POST")
-	router.HandleFunc("/task/{taskId}", controllers.GetTaskById).Methods("GET")
-	router.HandleFunc("/task/{taskId}", controllers.DeleteTaskById).Methods("DELETE")
-	router.HandleFunc("/task/{taskId}", controllers.EditTaskCompletedById).Methods("PUT")
-	router.HandleFunc("/task/{taskId}", controllers.EditTaskDescriptionByID).Methods("PUT")
+	router.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler)
+	router.HandleFunc("/api/task/", controllers.GetTask).Methods("GET")
+	router.HandleFunc("/api/task/", controllers.CreateTask).Methods("POST")
+	router.HandleFunc("/api/task/{taskId}", controllers.GetTaskById).Methods("GET")
+	router.HandleFunc("/api/task/{taskId}", controllers.DeleteTaskById).Methods("DELETE")
+	router.HandleFunc("/api/task/{taskId}", controllers.EditTaskCompletedById).Methods("PUT")
+	router.HandleFunc("/api/task/{taskId}", controllers.EditTaskDescriptionByID).Methods("PUT")
 }
